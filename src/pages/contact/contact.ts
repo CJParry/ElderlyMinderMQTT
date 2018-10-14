52486cb6c5698ba3e2b0783c93f209070a54d0c7
parrychri <parrychri1@ecs.vuw.ac.nz>
2018-10-15 12:55:42 +1300

    Styles tabs menu

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -11,20 +11,16 @@ export class ContactPage {
 
   }
 
-
-
-
 public barChartOptions:any = {
   scaleShowVerticalLines: false,
   responsive: true
 };
-public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+public barChartLabels:string[] = ['Living', 'Kitchen', 'Dining', 'Bedroom', 'Toilet'];
 public barChartType:string = 'bar';
 public barChartLegend:boolean = true;
 
 public barChartData:any[] = [
-  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  {data: [28, 48, 40, 19, 86], label: 'Voltage'}
 ];
 
 // events
@@ -36,24 +32,8 @@ public chartHovered(e:any):void {
   console.log(e);
 }
 
-public randomize():void {
-  // Only Change 3 values
-  let data = [
-    Math.round(Math.random() * 100),
-    59,
-    80,
-    (Math.random() * 100),
-    56,
-    (Math.random() * 100),
-    40];
-  let clone = JSON.parse(JSON.stringify(this.barChartData));
-  clone[0].data = data;
-  this.barChartData = clone;
-  /**
-   * (My guess), for Angular to recognize the change in the dataset
-   * it has to change the dataset variable directly,
-   * so one way around it, is to clone the data, change it and then
-   * assign it;
-   */
+public update():void {
+
+
 }
 }
