fb2e4df2ea125b32032b28e98be77a8140c07ecd
parrychri <parrychri1@ecs.vuw.ac.nz>
2018-10-15 12:38:39 +1300

    Added pie chart

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+// Doughnut
+public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+public doughnutChartData:number[] = [350, 450, 100];
+public doughnutChartType:string = 'doughnut';
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  // events
+public chartClicked(e:any):void {
+  console.log(e);
 }
+
+public chartHovered(e:any):void {
+  console.log(e);
+}
+}
+
+
+
+
